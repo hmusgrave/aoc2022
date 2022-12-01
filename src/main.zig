635317eb7315aba925c2e10702b79e75ad9acd57
Hans Musgrave <Hans.Musgrave@gmail.com>
2022-11-30 22:04:30 -0800

    day 1 star 2

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,45 @@ const std = @import("std");
 
 const data = @embedFile("input.txt");
 
+fn Priority(comptime T: type, comptime N: usize) type {
+    return struct {
+        data: [N]T,
+        count: usize,
+
+        pub fn init() @This() {
+            return .{
+                .data = [_]T{0} ** N,
+                .count = 0,
+            };
+        }
+
+        pub fn update(self: *@This(), val: T) void {
+            if (self.count < N) {
+                self.data[self.count] = val;
+                self.count += 1;
+            } else {
+                var m = self.data[0];
+                var mi: usize = 0;
+                for (self.data[1..]) |x, i| {
+                    if (x < m) {
+                        m = x;
+                        mi = i + 1;
+                    }
+                }
+                if (val > m)
+                    self.data[mi] = val;
+            }
+        }
+
+        pub fn all(self: @This()) T {
+            var total: T = 0;
+            for (self.data) |x|
+                total += x;
+            return total;
+        }
+    };
+}
+
 pub fn main() !void {
     std.debug.print("AoC 2022 Day 1!!!\n", .{});
 
@@ -10,19 +49,16 @@ pub fn main() !void {
     const allocator = gp.allocator();
     _ = allocator;
 
-    var max: ?usize = 0;
+    var max = Priority(usize, 3).init();
     var elf_iter = std.mem.split(u8, data, "\n\n");
-    while (elf_iter.next()) |x| {
+    var i: usize = 0;
+    while (elf_iter.next()) |x| : (i += 1) {
         var total: usize = 0;
         var cal_iter = std.mem.tokenize(u8, x, "\n");
         while (cal_iter.next()) |y| {
             total += try std.fmt.parseInt(usize, y, 10);
         }
-        if (max) |m| {
-            max = @max(m, total);
-        } else {
-            max = total;
-        }
+        max.update(total);
     }
-    std.debug.print("max: {any}\n", .{max});
+    std.debug.print("max: {any}\n", .{max.all()});
 }
