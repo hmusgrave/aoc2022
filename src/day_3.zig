18216cf310e61262bea1c0219fc15025ab24a420
Hans Musgrave <Hans.Musgrave@gmail.com>
2022-12-03 22:43:39 -0800

    day 3 star 1

---
+++ src/day_3.zig
@@ -0,0 +1,72 @@
+const std = @import("std");
+const utils = @import("./utils.zig");
+
+const Assignment = struct {
+    start: usize,
+    end: usize,
+
+    pub fn parse(data: anytype) !@This() {
+        var idx = std.mem.split(u8, data, "-");
+        return @This(){
+            .start = try std.fmt.parseInt(usize, idx.next() orelse {
+                return error.IncompleteData;
+            }, 10),
+            .end = try std.fmt.parseInt(usize, idx.next() orelse {
+                return error.IncompleteData;
+            }, 10),
+        };
+    }
+
+    pub fn contains(self: @This(), other: @This()) bool {
+        return self.start <= other.start and self.end >= other.end;
+    }
+};
+
+const AssignmentPair = struct {
+    elves: [2]Assignment,
+
+    pub fn parse(data: anytype) !@This() {
+        var elves = std.mem.split(u8, data, ",");
+        return @This(){
+            .elves = [2]Assignment{
+                try Assignment.parse(elves.next() orelse {
+                    return error.IncompleteData;
+                }),
+                try Assignment.parse(elves.next() orelse {
+                    return error.IncompleteData;
+                }),
+            },
+        };
+    }
+
+    pub fn contains(self: @This()) bool {
+        const elves = self.elves;
+        return elves[0].contains(elves[1]) or elves[1].contains(elves[0]);
+    }
+};
+
+fn solve0(data: []u8) !usize {
+    var lines = std.mem.split(u8, data, "\n");
+    var total: usize = 0;
+    while (lines.next()) |line| {
+        if (line.len == 0)
+            continue;
+        var pair = try AssignmentPair.parse(line);
+        if (pair.contains())
+            total += 1;
+    }
+    return total;
+}
+
+const solve1 = solve0;
+
+pub fn main() !void {
+    var gp = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gp.deinit();
+    const allocator = gp.allocator();
+
+    const data = try utils.data(allocator);
+    defer allocator.free(data);
+
+    std.debug.print("{}\n{}\n", .{ try solve0(data), try solve1(data) });
+}
